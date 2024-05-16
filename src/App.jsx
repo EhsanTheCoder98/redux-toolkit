c4584cce7f941f18efbf14529a29f94fe58f9855
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2024-05-16 20:01:53 +0330

    using useSelector and useDispatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { increament,decreament } from './features/counter/counterSlice';
 
 const App = () => {
+  const counter = useSelector(store => store.counter.counterValue);
+ const dispatch = useDispatch();
   return (
     <div>
-      Counter
+      Counter is:{counter}
+      <button onClick={()=>dispatch(increament())}>increament</button>
+      <button onClick={()=>dispatch(decreament())}>decreament</button>
     </div>
   );
 };
