8246b11bfc5919d5301fab54b5bacbae2482910c
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2024-05-16 21:38:19 +0330

    middleware used

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "../features/counter/counterSlice";
 import numberSlice from "../features/number/numberSlice";
+import logger from "redux-logger";
 
 const store = configureStore({
   reducer: { counter: counterSlice ,number:numberSlice },
+  middleware:getDefaultMiddleware=>getDefaultMiddleware().concat(logger)
 });
 export default store;
